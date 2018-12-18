283b615ac9843e324cbe1b4ac06bf745ad6c5306
Aidy <aidyszh@gmail.com>
2018-12-18 16:14:37 +0800

    create project using curl: curl start.spring.io/starter.zip -o JustGifIt.zip -d applicationName=JustGifIt -d artifactId=JustGifIt -d groupId=org.aidy

---
+++ Spring_Boot/JustGifIt/src/main/java/org/aidy/JustGifIt/JustGifIt.java
@@ -0,0 +1,14 @@
+package org.aidy.JustGifIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustGifIt {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustGifIt.class, args);
+	}
+
+}
+
